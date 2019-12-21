5acd1387452cc4f4cd7c8515ae3781c5a0532908
danielcampos <ddregalo@gmail.com>
2019-12-21 22:14:40 +0000

    Fix style warnings

--- helloworld.rb
+++ helloworld.rb
@@ -1,92 +1,93 @@
-#gems t
+# gems t
 
-puts("dicks")
+puts('dicks')
 
-#methods
-#while loop, create a condition that when met, keep the loop going, else end the loop. 
+# methods
+# while loop, create a condition that when met, keep the loop going, else end the loop. 
 
 def get_number()
-    return gets.chomp
+  return gets.chomp
 end
 
 def get_yes_no()
-    return gets.chomp
+  return gets.chomp
 end
 
 def check_input_is_number(input)
-    is_number = false
-    if input =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
-        is_number = true
-    end
-    puts("This is a juicy number")
-    return is_number
+  is_number = false
+  if input =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
+    is_number = true
+  end
+  puts('This is a juicy number')
+  return is_number
 end
 
 def abuse_and_exit()
-    puts("*************You trying to break me? Not in my house.")
-    exit
+  puts("*************You trying to break me? Not in my house.")
+  exit
 end
 
 # Ruby program to illustrate 'until' loop 
-  
-sessionValue = TRUE
-  
-# using until loop 
-# here do is optional 
-until sessionValue == FALSE do
-  
+session_value = TRUE
+
+# using until loop
+# here do is optional
+until session_value == FALSE do
 
 puts("What is the first number you would like to use?")
-aNumber = get_number()
-if check_input_is_number(aNumber)
-    aNumber = aNumber.to_f
-else 
-    abuse_and_exit()
-end 
+
+a_number = get_number()
+
+if check_input_is_number(a_number)
+  a_number = a_number.to_f
+else
+  abuse_and_exit()
+end
 
 puts("What is the second number you would like to use?")
 
-bNumber = get_number()
-if check_input_is_number(bNumber)
-    bNumber = bNumber.to_f
-else 
-    abuse_and_exit()
-end 
+b_number = get_number()
+if check_input_is_number(b_number)
+  b_number = b_number.to_f
+else
+  abuse_and_exit()
+end
 
 puts("PLEAAASSEE sir, choose a computation to complete \n
-    1. #{aNumber} + #{bNumber} \n 
-    2. #{aNumber} - #{bNumber} \n
-    3. #{aNumber} / #{bNumber} \n
-    4. #{aNumber} x #{bNumber} \n") 
+  1. #{a_number} + #{b_number} \n
+  2. #{a_number} - #{b_number} \n
+  3. #{a_number} / #{b_number} \n
+  4. #{a_number} x #{b_number} \n")
 
-userOption = gets.chomp
+user_option = gets.chomp
 
-case userOption 
-when "1"
-    puts("You have selected option 1")
-    answer = aNumber + bNumber 
-when "2"
-    puts("You have selected option 2")
-    answer = aNumber - bNumber 
+case user_option
+when '1'
+  puts('You have selected option 1')
+  answer = a_number + b_number
+when '2'
+  puts('You have selected option 2')
+  answer = a_number - b_number
 
-when "3"
-    puts("You have selected option 3")
-    answer = aNumber / bNumber 
+when '3'
+  puts('You have selected option 3')
+  answer = a_number / b_number
 
-when "4"
-    puts("You have selected option 4")
-    answer = aNumber * bNumber 
-end 
-puts("Your answer is:")
+when '4'
+  puts('You have selected option 4')
+  answer = a_number * b_number
+end
+
+puts('Your answer is:')
 puts(answer)
 puts("Would you like to do that again?")
 
-userYesno = get_yes_no()
+user_yesno = get_yes_no()
 
-if userYesno = "yes"
+if user_yesno == "yes"
    # sessionValue = TRUE
-else sessionValue = FALSE
+else session_value = FALSE
     puts("you put no")
-end 
-# here loop ends 
+end
+  # here loop ends
 end
